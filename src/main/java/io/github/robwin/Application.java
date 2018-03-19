6c3dd86b0edf8acda6ed604f5c022e550d08ba32
Robert Winkler <rwinkler@telekom.de>
2018-03-19 11:10:54 +0100

    Updated readme

--- src/main/java/io/github/robwin/Application.java
+++ src/main/java/io/github/robwin/Application.java
@@ -2,13 +2,8 @@ package io.github.robwin;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.annotation.Bean;
-
-import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
-import io.github.resilience4j.circuitbreaker.monitoring.health.CircuitBreakerHealthIndicator;
 
 @SpringBootApplication
 @EnableConfigurationProperties
@@ -16,15 +11,5 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
-	@Bean
-	public HealthIndicator backendA(CircuitBreakerRegistry circuitBreakerRegistry){
-		return new CircuitBreakerHealthIndicator(circuitBreakerRegistry.circuitBreaker("backendA"));
-	}
-
-	@Bean
-	public HealthIndicator backendB(CircuitBreakerRegistry circuitBreakerRegistry){
-		return new CircuitBreakerHealthIndicator(circuitBreakerRegistry.circuitBreaker("backendB"));
 	}
 }
