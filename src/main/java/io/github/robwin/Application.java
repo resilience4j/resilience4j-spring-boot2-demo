fa6a547cc4f60a0f4939960ac70924a9a49984eb
Robert Winkler <rwinkler@telekom.de>
2018-03-19 14:09:14 +0100

    Updated readme

--- src/main/java/io/github/robwin/Application.java
+++ src/main/java/io/github/robwin/Application.java
@@ -3,10 +3,8 @@ package io.github.robwin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties
 public class Application {
 
 	public static void main(String[] args) {
