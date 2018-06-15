95dcee9729e808515ffba08508943be47d8f3318
Raffaele Litto <rLitto@users.noreply.github.com>
2018-06-15 23:10:19 +0100

    Updated example configuration

---
+++ src/main/java/io/github/robwin/exception/RecordFailurePredicate.java
@@ -0,0 +1,10 @@
+package io.github.robwin.exception;
+
+import java.util.function.Predicate;
+
+public class RecordFailurePredicate implements Predicate<Throwable> {
+    @Override
+    public boolean test(Throwable throwable) {
+        return !(throwable instanceof BusinessException);
+    }
+}
