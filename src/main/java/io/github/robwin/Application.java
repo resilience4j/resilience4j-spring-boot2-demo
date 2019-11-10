203222a4d75f5c232202a8147cad85b115166c65
Tomasz Skowroński <>
2019-11-10 12:27:09 +0100

    Redirect to /actuator added.

--- src/main/java/io/github/robwin/Application.java
+++ src/main/java/io/github/robwin/Application.java
@@ -1,13 +1,28 @@
 package io.github.robwin;
 
 
+import java.net.URI;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.permanentRedirect;
 
 @SpringBootApplication
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    RouterFunction<ServerResponse> redirectRoot() {
+        return route(GET("/"),
+                req -> permanentRedirect(URI.create("/actuator")).build());
+    }
 }
