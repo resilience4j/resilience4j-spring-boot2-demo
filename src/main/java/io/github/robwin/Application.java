5214fa1207354b5db2325eeb114fd11f1466fe64
Robert Winkler <rwinkler@telekom.de>
2020-10-26 14:56:43 +0100

    Added RegistryEventConsumer examples

--- src/main/java/io/github/robwin/Application.java
+++ src/main/java/io/github/robwin/Application.java
@@ -3,6 +3,14 @@ package io.github.robwin;
 
 import java.net.URI;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.core.registry.EntryAddedEvent;
+import io.github.resilience4j.core.registry.EntryRemovedEvent;
+import io.github.resilience4j.core.registry.EntryReplacedEvent;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+import io.github.resilience4j.retry.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -16,6 +24,8 @@ import static org.springframework.web.reactive.function.server.ServerResponse.pe
 @SpringBootApplication
 public class Application {
 
+    private static Logger LOG = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
@@ -25,4 +35,47 @@ public class Application {
         return route(GET("/"),
                 req -> permanentRedirect(URI.create("/actuator")).build());
     }
+
+    @Bean
+    public RegistryEventConsumer<CircuitBreaker> myRegistryEventConsumer() {
+
+        return new RegistryEventConsumer<CircuitBreaker>() {
+            @Override
+            public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
+                entryAddedEvent.getAddedEntry().getEventPublisher().onEvent(event -> LOG.info(event.toString()));
+            }
+
+            @Override
+            public void onEntryRemovedEvent(EntryRemovedEvent<CircuitBreaker> entryRemoveEvent) {
+
+            }
+
+            @Override
+            public void onEntryReplacedEvent(EntryReplacedEvent<CircuitBreaker> entryReplacedEvent) {
+
+            }
+        };
+    }
+
+@Bean
+public RegistryEventConsumer<Retry> myRetryRegistryEventConsumer() {
+
+    return new RegistryEventConsumer<Retry>() {
+        @Override
+        public void onEntryAddedEvent(EntryAddedEvent<Retry> entryAddedEvent) {
+            entryAddedEvent.getAddedEntry().getEventPublisher().onEvent(event -> LOG.info(event.toString()));
+        }
+
+        @Override
+        public void onEntryRemovedEvent(EntryRemovedEvent<Retry> entryRemoveEvent) {
+
+        }
+
+        @Override
+        public void onEntryReplacedEvent(EntryReplacedEvent<Retry> entryReplacedEvent) {
+
+        }
+    };
+}
+
 }
