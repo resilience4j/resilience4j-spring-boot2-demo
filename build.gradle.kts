3f53c88e6aa858478113c6353f492cc9f2c71fe4
Robert Winkler <rwinkler@telekom.de>
2022-11-21 10:46:53 +0100

    Updated to resilience4j 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 
 plugins {
     application
-    id("org.springframework.boot") version ("2.7.4")
+    id("org.springframework.boot") version ("2.7.5")
     id("io.spring.dependency-management") version "1.0.14.RELEASE"
 }
 
@@ -11,6 +11,13 @@ java {
     }
 }
 
+apply(plugin = "io.spring.dependency-management")
+
+the<io.spring.gradle.dependencymanagement.dsl.DependencyManagementExtension>().apply {
+    imports {
+        mavenBom("io.github.resilience4j:resilience4j-bom:2.0.0")
+    }
+}
 
 repositories {
 	mavenCentral()
@@ -24,26 +31,27 @@ repositories {
 //	resolutionStrategy.cacheChangingModulesFor 0, "seconds"
 //}
 
-val resilience4jVersion = "1.7.1"
+val resilience4jVersion = "2.0.0"
 
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
 
 dependencies {
+
     implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-webflux")
 	implementation("org.springframework.boot:spring-boot-starter-actuator")
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 
-	implementation("io.github.resilience4j:resilience4j-spring-boot2:${resilience4jVersion}")
-	implementation("io.github.resilience4j:resilience4j-all:${resilience4jVersion}") // Optional, only required when you want to use the Decorators class
-	implementation("io.github.resilience4j:resilience4j-reactor:${resilience4jVersion}")
+	implementation("io.github.resilience4j:resilience4j-spring-boot2")
+	implementation("io.github.resilience4j:resilience4j-all") // Optional, only required when you want to use the Decorators class
+	implementation("io.github.resilience4j:resilience4j-reactor")
 	implementation("io.micrometer:micrometer-registry-prometheus")
 
-	implementation("de.codecentric:chaos-monkey-spring-boot:2.1.1")
+	implementation("de.codecentric:chaos-monkey-spring-boot:2.6.1")
 	
-	implementation("io.vavr:vavr-jackson:0.10.0")
+	implementation("io.vavr:vavr-jackson:0.10.3")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
